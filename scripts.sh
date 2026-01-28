6da92c2fb5f8e9862b22a986c12b3611499f7360
jianfeng <2285809071@qq.com>
2026-01-28 13:45:17 +0800

    Update and rename test.sh to scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,6 @@
+python train.py --save_path ./ckpt/DP_CLIP 
 declare -a dataset=(MVTec MPDD BTAD Colon_clinicDB Colon_colonDB Colon_Kvasir Colon_cvc300 Liver Brain Retina)
-save_path="./ckpt/test"
-
+save_path="./ckpt/DP_CLIP"
 for i in "${dataset[@]}"; do
-    python testUP.py --save_path $save_path --dataset $i 
+    python test.py --save_path $save_path --dataset $i 
 done

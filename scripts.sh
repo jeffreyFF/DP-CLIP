731d81db3cc73298638905d53cce6ade3b25328f
jianfeng <2285809071@qq.com>
2026-01-28 20:56:32 +0800

    scripts.sh

--- scripts.sh
+++
@@ -1,6 +0,0 @@
-python train.py --save_path ./ckpt/DP_CLIP 
-declare -a dataset=(MVTec MPDD BTAD Colon_clinicDB Colon_colonDB Colon_Kvasir Colon_cvc300 Liver Brain Retina)
-save_path="./ckpt/DP_CLIP"
-for i in "${dataset[@]}"; do
-    python test.py --save_path $save_path --dataset $i 
-done
